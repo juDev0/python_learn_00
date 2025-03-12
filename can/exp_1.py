c8a08e637fb94490516660db9d0dc4ef1b0c4713
juDev0 <udiajoshua@gmail.com>
2025-03-12 14:55:03 -0800

    after the begining

---
+++ can/exp_1.py
@@ -0,0 +1,14 @@
+#Just testing my knowledge 
+print("i love \n")
+print("""university of uyo
+it has such, being a great time there,
+i thank GOD\n""")
+name= input("what is your name ?\n")
+print("yes indeed it realy has being \'GREAT\'\n")
+print(('yes indeed it realy has being \" GREAT \" '+ name +"\n")*5)
+print ("hi \n"*5)
+""""so thats what we have
+as for now"""
+#now lets try the spacing parameter
+print("hello          " , sep="***",end="")
+print("who is there "+ name +" go \"cheak\" it out")
---
+++ can/lo.py
@@ -0,0 +1,12 @@
+print("Hello")
+print("\nMy name is joshua")
+print("""\nMy name is joshua
+my name is not jhon \n""")
+a=4
+print(("How "+"Are you "+"Today.""\n") * a)
+name= input("Hello what is your name?\n")
+print((name+"\n")*5) #to print  only the name
+print("nice to meet you "+name)
+"""this is mr just trying multi line comment
+ """
+print (("ok "+name)*3)
