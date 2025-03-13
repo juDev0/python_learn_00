3565b4cd94c7a4bb23b2bd9311a52d82e39347e2
juDev0 <udiajoshua@gmail.com>
2025-03-13 11:56:47 -0800

    calculator creat

---
+++ just/calculator.py
@@ -0,0 +1,20 @@
+#building calculator
+x = input("what's x? ").strip()
+y = input("what's y? ").strip()
+
+print(int(x) + int(y))
+
+'''one is a way to solve it
+the other, is another way to solve it'''
+
+a = int(5)
+b = int(5)
+print(a + b)
+
+'''one is a way to solve it
+the other, is another way to solve it'''
+
+i = int(input("what's i? ").strip())
+j = int(input("what's j? ").strip())
+
+print(i + j)
