fc063795bf4c971eaf9e41eab531be1acce384b0
juDev0 <udiajoshua@gmail.com>
2025-03-13 15:30:55 -0800

    definitions

--- just/calculator.py
+++ just/calculator.py
@@ -1,4 +1,6 @@
 #building calculator
+
+#FIRST INT DATATYPE
 x = input("what's x? ").strip()
 y = input("what's y? ").strip()
 
@@ -18,3 +20,16 @@ i = int(input("what's i? ").strip())
 j = int(input("what's j? ").strip())
 
 print(i + j)
+
+#FLOT DATATYPE
+any_1 = input("enter any number").strip()
+any_2 = input("enter sec numbrer").strip()
+z = float(any_1)/float(any_2) 
+m = float(any_1)/float(any_2)
+m = round(m,2)
+
+#rounds up z by formating to 2fig
+print(f"{z:.2f}")
+
+#rounds up z by the use of the round fnc
+print(m)
--- just/exp_2.py
+++ just/exp_2.py
@@ -1,10 +1,1 @@
-print("""\ni am iron man 
-No, im tony stark 
-No, i am poopy""")
-#we make it happen with out the multi string 
-
-#concatinate it
-print("\ni am iron man " "No, im tony stark " "No, i am poopy")
-print("\ni am iron man \n" "No, im tony stark \n" "No, i am poopy")
-
-print("\nyes you i am \n" * 10 )
+# defining a functions
